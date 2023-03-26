d0c2b845c7caf2082fe72bcf9f86f51cd3df2b51
David Zhang <dzz0615@gmail.com>
2023-03-25 17:37:05 -0700

    added chat

---
+++ next/src/pages/api/chat.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type RequestData = {
+  messages: string[];
+};
+
+type ResponseData = {
+  messages: string[];
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  const data: RequestData = req.body;
+  res.status(200).json({ messages: [...data.messages, 'hello'] });
+}
